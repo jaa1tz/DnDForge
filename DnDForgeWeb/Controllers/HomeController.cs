16b46804e1676223de3d8150f31df2ba4491ab2b
jaa1tz <138750155+jaa1tz@users.noreply.github.com>
2023-09-02 11:51:32 -0500

    Initial commit

---
+++ DnDForgeWeb/Controllers/HomeController.cs
@@ -0,0 +1,49 @@
+﻿using DnDForgeWeb.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace DnDForgeWeb.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet("login")]
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost("login")]
+        public IActionResult LoginPost(LoginViewModel vm)
+        {
+            return View("Login");
+        }
+
+        public IActionResult Register()
+        {
+            return View();
+        }
+
+        public IActionResult CreateCharacter()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
